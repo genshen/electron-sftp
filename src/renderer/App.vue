c250544aada4f8270ce12f30e11ee2ebf657f37e
genshen <genshenchu@gmail.com>
2019-10-30 19:29:47 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Hi</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-sftp'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
